2302ebd4d407f7230e632b5992b5360da5b744d3
schel-d <schel.d.dev@gmail.com>
2023-07-09 18:07:43 +1000

    setup sass, reset prettier rules, remove vue pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <a href="/about" class="bacon"><p>HomeView</p></a>
   </main>
 </template>
+
+<style lang="scss" scoped>
+@use "@/assets/css-template/import" as template;
+.bacon {
+  @include template.button-filled;
+}
+</style>
