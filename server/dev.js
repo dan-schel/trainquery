cd20d5cc14476cdacc7f77288f03ccca863925d4
schel-d <schel.d.dev@gmail.com>
2023-07-12 11:48:42 +1000

    prod and dev servers

---
+++ server/dev.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { createSsrServer } = require("vite-ssr/dev");
+
+async function createServer() {
+  const app = express();
+
+  // Create vite-ssr server in middleware mode.
+  const viteServer = await createSsrServer({
+    server: { middlewareMode: true, appType: "custom" },
+  });
+
+  // Use vite's connect instance as middleware
+  app.use(viteServer.middlewares);
+
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+}
+
+createServer();
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  ssr: { format: 'cjs' },
   plugins: [
     viteSSR(),
     vue(),
