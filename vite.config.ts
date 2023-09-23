d7c4b61b182777631ac0a6babdbccefff7bad211
schel-d <schel.d.dev@gmail.com>
2023-09-23 22:19:14 +1000

    re-enable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,17 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import viteSSR from "vite-ssr/plugin.js";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   ssr: { format: "cjs" },
   plugins: [
-    //VitePWA({ registerType: "autoUpdate" }),
+    VitePWA({
+      registerType: "autoUpdate", workbox: {
+        globPatterns: ['**/*.{js,css}']
+      }
+    }),
     viteSSR(),
     vue(),
     vueJsx(),
