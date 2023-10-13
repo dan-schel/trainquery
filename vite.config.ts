d6ebec08074ea9d50744e23b6bf75a54f7e284a1
schel-d <schel.d.dev@gmail.com>
2023-10-13 18:18:31 +1100

    everything needed for pwa theoretically

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,28 @@ export default defineConfig({
   ssr: { format: "cjs" },
   plugins: [
     VitePWA({
-      registerType: "autoUpdate", workbox: {
+      registerType: "autoUpdate",
+      workbox: {
         globPatterns: ['**/*.{js,css}']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'TrainQuery',
+        short_name: 'TrainQuery',
+        description: "Navigate Melbourne's train network",
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     }),
     viteSSR(),
