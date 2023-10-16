d28825fc8b0c43db108efe89b2d5e20934a2d8b2
schel-d <schel.d.dev@gmail.com>
2023-10-16 19:13:05 +1100

    gtfs feeds can have separate names to their paths

--- server/config/gtfs-config.ts
+++ server/config/gtfs-config.ts
@@ -9,7 +9,7 @@ export class GtfsPersistenceConfig {
     readonly mongoUrl: string,
     readonly refresh: RefreshPolicy,
     readonly refreshSeconds: number
-  ) {}
+  ) { }
 
   static readonly json = z
     .object({
@@ -26,14 +26,17 @@ export class GtfsConfig {
   constructor(
     readonly staticUrl: string,
     readonly persist: GtfsPersistenceConfig | null,
-    readonly subfeeds: string[] | null
-  ) {}
+    readonly subfeeds: { name: string, path: string }[] | null
+  ) { }
 
   static readonly json = z
     .object({
       staticUrl: z.string(),
       persist: GtfsPersistenceConfig.json.nullable(),
-      subfeeds: z.string().array().optional(),
+      subfeeds: z.object({
+        name: z.string(),
+        path: z.string(),
+      }).array().optional(),
     })
     .transform(
       (x) => new GtfsConfig(x.staticUrl, x.persist, x.subfeeds ?? null)
