9b08bd4cedfd0d48a2e48a2d0738e90b4e4cdb2e
schel-d <schel.d.dev@gmail.com>
2023-11-16 13:36:01 +1100

    fetch all gtfs-r apis

--- server/gtfs/realtime/fetch.ts
+++ server/gtfs/realtime/fetch.ts
@@ -2,15 +2,25 @@ import fetch from "node-fetch";
 import { transit_realtime } from "./proto";
 import fsp from "fs/promises";
 
-export async function fetchRealtime(): Promise<transit_realtime.FeedMessage> {
-  const subscriptionKey = process.env.GTFS_REALTIME_KEY;
+export async function fetchRealtime() {
+  const key = process.env.GTFS_REALTIME_KEY;
 
-  if (subscriptionKey == null) {
+  if (key == null) {
     throw new Error("GTFS_REALTIME_KEY environment variable not set.");
   }
 
+  await fetchToFile("tripupdates", key, "_trip-updates.txt");
+  await fetchToFile("servicealerts", key, "_service-alerts.txt");
+  await fetchToFile("vehicleposition-updates", key, "_vehicle-positions.txt");
+}
+
+async function fetchToFile(
+  api: string,
+  subscriptionKey: string,
+  file: string,
+): Promise<transit_realtime.FeedMessage> {
   const response = await fetch(
-    "https://data-exchange-api.vicroads.vic.gov.au/opendata/v1/gtfsr/metrotrain-tripupdates",
+    `https://data-exchange-api.vicroads.vic.gov.au/opendata/v1/gtfsr/metrotrain-${api}`,
     {
       headers: {
         "Ocp-Apim-Subscription-Key": subscriptionKey,
@@ -22,7 +32,7 @@ export async function fetchRealtime(): Promise<transit_realtime.FeedMessage> {
   const bytes = new Uint8Array(buffer);
   const message = transit_realtime.FeedMessage.decode(bytes);
 
-  await fsp.writeFile("output.txt", JSON.stringify(message.toJSON()));
+  await fsp.writeFile(file, JSON.stringify(message.toJSON()));
 
   return message;
 }
