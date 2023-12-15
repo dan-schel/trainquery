5a77947b3be97b63d8fad3ca44473525903a54d2
schel-d <schel.d.dev@gmail.com>
2023-12-15 16:00:31 +1100

    return sorted trip updates, alerts, and positions

--- server/gtfs/realtime/fetch.ts
+++ server/gtfs/realtime/fetch.ts
@@ -1,17 +1,48 @@
 import fetch from "node-fetch";
 import { transit_realtime } from "./proto";
 import fsp from "fs/promises";
+import path from "path";
+import { nonNull } from "@schel-d/js-utils";
 
-export async function fetchRealtime() {
+export type GtfsRealtimeData = {
+  tripUpdates: transit_realtime.ITripUpdate[];
+  serviceAlerts: transit_realtime.IAlert[];
+  vehiclePositions: transit_realtime.IVehiclePosition[];
+};
+
+export async function fetchRealtime(): Promise<GtfsRealtimeData> {
   const key = process.env.GTFS_REALTIME_KEY;
 
   if (key == null) {
     throw new Error("GTFS_REALTIME_KEY environment variable not set.");
   }
 
-  await fetchToFile("tripupdates", key, "_trip-updates.json");
-  await fetchToFile("servicealerts", key, "_service-alerts.json");
-  await fetchToFile("vehicleposition-updates", key, "_vehicle-positions.json");
+  const tripUpdates = await fetchToFile(
+    "tripupdates",
+    key,
+    "data-output/trip-updates.txt",
+  );
+  const serviceAlerts = await fetchToFile(
+    "servicealerts",
+    key,
+    "data-output/service-alerts.txt",
+  );
+  const vehiclePositions = await fetchToFile(
+    "vehicleposition-updates",
+    key,
+    "data-output/vehicle-positions.txt",
+  );
+
+  const combined = [
+    ...tripUpdates.entity,
+    ...serviceAlerts.entity,
+    ...vehiclePositions.entity,
+  ];
+  return {
+    tripUpdates: combined.map((e) => e.tripUpdate ?? null).filter(nonNull),
+    serviceAlerts: combined.map((e) => e.alert ?? null).filter(nonNull),
+    vehiclePositions: combined.map((e) => e.vehicle ?? null).filter(nonNull),
+  };
 }
 
 async function fetchToFile(
@@ -32,6 +63,7 @@ async function fetchToFile(
   const bytes = new Uint8Array(buffer);
   const message = transit_realtime.FeedMessage.decode(bytes);
 
+  await fsp.mkdir(path.dirname(file), { recursive: true });
   await fsp.writeFile(file, JSON.stringify(message.toJSON(), null, 2));
 
   return message;
