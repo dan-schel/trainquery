264a86c980239ff7eb922d3bdea4359ac4401379
schel-d <schel.d.dev@gmail.com>
2023-12-21 14:42:55 +1100

    show stop ids on line page in dev mode

--- src/components/line-diagram/LinePageStop.vue
+++ src/components/line-diagram/LinePageStop.vue
@@ -4,27 +4,40 @@ import { requireStop } from "shared/system/config-utils";
 import { computed } from "vue";
 import { getStopPageRoute } from "shared/system/config-utils";
 import type { LineDiagramStop } from "shared/system/routes/line-routes";
+import { useSettings } from "@/settings/settings";
 
 const props = defineProps<{
   stopData: LineDiagramStop<null, null>;
 }>();
 
 const stop = computed(() => requireStop(getConfig(), props.stopData.stop));
+
+const { settings } = useSettings();
+const developerMode = computed(() => settings.value?.developerMode ?? false);
 </script>
 
 <template>
-  <p>
-    <RouterLink
-      class="link"
-      :class="{ express: stopData.express }"
-      :to="getStopPageRoute(getConfig(), stop.id, null, null)"
-      >{{ stopData.express ? "Skips " : "" }}{{ stop.name }}</RouterLink
-    >
-  </p>
+  <div>
+    <p>
+      <RouterLink
+        class="link"
+        :class="{ express: stopData.express }"
+        :to="getStopPageRoute(getConfig(), stop.id, null, null)"
+      >
+        {{ stopData.express ? "Skips " : "" }}{{ stop.name }}
+      </RouterLink>
+    </p>
+    <p v-if="developerMode" class="stop-id">
+      <code>#{{ stop.id }}</code>
+    </p>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @use "@/assets/css-template/import" as template;
+div {
+  @include template.row;
+}
 .link {
   --color-accent: var(--color-ink-100);
   font-weight: bold;
@@ -35,5 +48,8 @@ const stop = computed(() => requireStop(getConfig(), props.stopData.stop));
     font-size: 0.8rem;
     font-style: italic;
   }
+}
+.stop-id {
+  margin-left: 0.5rem;
 }
 </style>
