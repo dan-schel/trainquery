d22d5b2a95e0a279e571ef304f7b616440f33a2b
schel-d <schel.d.dev@gmail.com>
2024-07-19 21:15:07 +1000

    introduce route stop types

--- shared/system/routes/line-route.ts
+++ shared/system/routes/line-route.ts
@@ -115,54 +115,115 @@ export class DirectionDefinition {
   }
 }
 
+/** All route stop types. */
+export const RouteStopTypes = [
+  // Considered a stop on the line. Not all trains necessarily stop here, but
+  // "stopping all stations" trains would.
+  "stops",
+  // Still considered "a stop on the line", but line diagrams show this stop
+  // greyed out, acknowledging that it's always skipped.
+  "express",
+  // A stop that isn't considered to be "on this line" at all, i.e. a service
+  // can skip this stop and still be considered "stopping all stations", e.g.
+  // suburban stations on V/Line trains. Only shown on the line page as skipped.
+  "via",
+  // Like "via", but suddenly is considered to be "on this line" for a service
+  // that provides a stopping time, e.g. East Pakenham on the Gippsland line.
+  "viaish",
+] as const;
+/** E.g. 'stops', 'via', 'express', etc. */
+export type RouteStopType = (typeof RouteStopTypes)[number];
+/** Matches a value in {@link RouteStopTypes}. */
+export const RouteStopTypeJson = z.enum(RouteStopTypes);
+
 /**
  * Describes stopping rules (e.g. express, set down only, etc.) for a particular
  * stop on a particular line.
  */
-export class RouteStop<T extends boolean = boolean> {
+export class RouteStop<T extends RouteStopType = RouteStopType> {
   constructor(
     /** Which stop occurs now on this route. */
     readonly stop: StopID,
     /** True if the route passes the stop without stopping. */
-    readonly via: T,
+    readonly type: T,
     /** Can passengers alight at this station? */
-    readonly setsDown: T extends false ? PusdoFilter : undefined,
+    readonly setsDown: T extends "stops" | "viaish" ? PusdoFilter : undefined,
     /** Can passengers board at this station? */
-    readonly picksUp: T extends false ? PusdoFilter : undefined,
+    readonly picksUp: T extends "stops" | "viaish" ? PusdoFilter : undefined,
   ) {}
 
   static readonly json = z
     .union([
       z.object({
         stops: StopIDJson,
+        express: z.undefined(),
         via: z.undefined(),
+        viaish: z.undefined(),
         setsDown: PusdoFilter.json.default(""),
         picksUp: PusdoFilter.json.default(""),
       }),
       z.object({
         stops: z.undefined(),
-        via: StopIDJson,
+        express: StopIDJson,
+        via: z.undefined(),
+        viaish: z.undefined(),
         setsDown: z.undefined(),
         picksUp: z.undefined(),
       }),
+      z.object({
+        stops: z.undefined(),
+        express: z.undefined(),
+        via: StopIDJson,
+        viaish: z.undefined(),
+        setsDown: z.undefined(),
+        picksUp: z.undefined(),
+      }),
+      z.object({
+        stops: z.undefined(),
+        express: z.undefined(),
+        via: z.undefined(),
+        viaish: StopIDJson,
+        setsDown: PusdoFilter.json.default(""),
+        picksUp: PusdoFilter.json.default(""),
+      }),
     ])
-    .transform(
-      (x) =>
-        new RouteStop(x.stops ?? x.via, x.via != null, x.setsDown, x.picksUp),
-    );
+    .transform((x) => {
+      if (x.stops != null) {
+        return new RouteStop(x.stops, "stops", x.setsDown, x.picksUp);
+      } else if (x.express != null) {
+        return new RouteStop(x.express, "express", undefined, undefined);
+      } else if (x.via != null) {
+        return new RouteStop(x.via, "via", undefined, undefined);
+      } else {
+        return new RouteStop(x.viaish, "viaish", x.setsDown, x.picksUp);
+      }
+    });
 
   toJSON(): z.input<typeof RouteStop.json> {
-    if (this.via === false) {
+    const pusdoFilters = {
+      setsDown: this.setsDown?.toJSON(),
+      picksUp: this.picksUp?.toJSON(),
+    };
+
+    if (this.type === "stops") {
       return {
         stops: this.stop,
-        setsDown: (this as RouteStop<false>).setsDown.toJSON(),
-        picksUp: (this as RouteStop<false>).picksUp.toJSON(),
+        ...pusdoFilters,
+      };
+    } else if (this.type === "express") {
+      return {
+        express: this.stop,
+      };
+    } else if (this.type === "via") {
+      return {
+        via: this.stop,
+      };
+    } else {
+      return {
+        viaish: this.stop,
+        ...pusdoFilters,
       };
     }
-
-    return {
-      via: this.stop,
-    };
   }
 }
 
