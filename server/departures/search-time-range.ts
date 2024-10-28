79c2883dd9e205fce214d74b43bd76551b9c68e1
dan-schel <schel.d.dev@gmail.com>
2024-10-28 17:25:32 +1100

    test build failure

--- server/departures/search-time-range.ts
+++ server/departures/search-time-range.ts
@@ -34,7 +34,7 @@ export function getSearchTimes(
     start1: QUtcDateTime,
     end1: QUtcDateTime,
     start2: QUtcDateTime,
-    end2: QUtcDateTime,
+    end2: QUtcDat eTime,
   ) => {
     if (end1.isBeforeOrEqual(start2)) {
       return null;
