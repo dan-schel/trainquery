fbbd25df2e3f1535c063c553a8d82cee7f0321e3
dan-schel <schel.d.dev@gmail.com>
2024-11-02 14:02:19 +1100

    silence sass nested declarations warning

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["mixed-decls"],
+      },
+    },
+  },
   ssr: { format: "cjs" },
   plugins: [
     VitePWA({
